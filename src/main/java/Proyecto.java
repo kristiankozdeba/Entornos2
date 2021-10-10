b663a8abb313e5bd88e1d6575582ac05aa78d7a8
kristian <kristian.kozdeba@educa.madrid.org>
2021-10-10 17:10:47 +0200

    Creacion de proyecto intellij

---
+++ src/main/java/Proyecto.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Proyecto {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Hola buenas tardes");
+    }
+}
